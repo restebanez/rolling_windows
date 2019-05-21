6d9740cf036b48eb4469c4fee71b2e2ff646ca62
Rodrigo Estebanez <rodhom@gmail.com>
2019-05-21 11:38:56 +0100

    remove older test

--- second_approach/rolling_window_spec.rb
+++ second_approach/rolling_window_spec.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'bundler'
+require "rspec/json_expectations" # https://relishapp.com/waterlink/rspec-json-expectations/docs/json-expectations/array-matching-support-for-include-json-matcher
+
 require 'time'
 
 require 'redis'
@@ -18,77 +20,116 @@ RSpec.describe "Rolling windows in Redis" do
 
     it 'returns the buckes names within the time range' do
       bucket_names = rolling_window.generate_bucket_names(epoch_since: epoch_since, epoch_to: epoch_to)
-      expect(bucket_names).to include(
-        include(span: 1.minute,  window_starts: Time.parse('2019-05-18 18:03:00 +0100')),
-        include(span: 1.minute,  window_starts: Time.parse('2019-05-18 18:04:00 +0100')),
-        include(span: 5.minutes, window_starts: Time.parse('2019-05-18 18:05:00 +0100')),
-        include(span: 5.minutes, window_starts: Time.parse('2019-05-18 18:10:00 +0100')))
+      puts JSON.pretty_generate(bucket_names)
+      expect(JSON.generate(bucket_names)).to include_json(
+        [
+          {
+            "window_starts": "2019-05-18 18:03:00 +0100",
+            "window_finishes": "2019-05-18 18:04:00 +0100",
+            "span": "60"
+          },
+          {
+            "window_starts": "2019-05-18 18:04:00 +0100",
+            "window_finishes": "2019-05-18 18:05:00 +0100",
+            "span": "60"
+          },
+          {
+              "window_starts": "2019-05-18 18:05:00 +0100",
+              "window_finishes": "2019-05-18 18:10:00 +0100",
+              "span": "300"
+          },
+          {
+              "window_starts": "2019-05-18 18:10:00 +0100",
+              "window_finishes": "2019-05-18 18:15:00 +0100",
+              "span": "300"
+          },
+          {
+              "window_starts": "2019-05-18 18:15:00 +0100",
+              "window_finishes": "2019-05-18 18:20:00 +0100",
+              "span": "300"
+          },
+          {
+              "window_starts": "2019-05-18 18:20:00 +0100",
+              "window_finishes": "2019-05-18 18:25:00 +0100",
+              "span": "300"
+          },
+          {
+              "window_starts": "2019-05-18 18:25:00 +0100",
+              "window_finishes": "2019-05-18 18:30:00 +0100",
+              "span": "300"
+          },
+          {
+              "window_starts": "2019-05-18 18:30:00 +0100",
+              "window_finishes": "2019-05-18 19:00:00 +0100",
+              "span": "1800"
+          },
+          {
+              "window_starts": "2019-05-18 19:00:00 +0100",
+              "window_finishes": "2019-05-18 19:30:00 +0100",
+              "span": "1800"
+          },
+          {
+              "window_starts": "2019-05-18 19:30:00 +0100",
+              "window_finishes": "2019-05-18 20:00:00 +0100",
+              "span": "1800"
+          },
+          {
+              "window_starts": "2019-05-18 20:00:00 +0100",
+              "window_finishes": "2019-05-18 20:30:00 +0100",
+              "span": "1800"
+          },
+          {
+              "window_starts": "2019-05-18 20:30:00 +0100",
+              "window_finishes": "2019-05-18 21:00:00 +0100",
+              "span": "1800"
+          },
+          {
+              "window_starts": "2019-05-18 21:00:00 +0100",
+              "window_finishes": "2019-05-19 00:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 00:00:00 +0100",
+              "window_finishes": "2019-05-19 03:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 03:00:00 +0100",
+              "window_finishes": "2019-05-19 06:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 06:00:00 +0100",
+              "window_finishes": "2019-05-19 09:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 09:00:00 +0100",
+              "window_finishes": "2019-05-19 12:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 12:00:00 +0100",
+              "window_finishes": "2019-05-19 15:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 15:00:00 +0100",
+              "window_finishes": "2019-05-19 18:00:00 +0100",
+              "span": "10800"
+          },
+          {
+              "window_starts": "2019-05-19 18:00:00 +0100",
+              "window_finishes": "2019-05-19 18:01:00 +0100",
+              "span": "60"
+          },
+          {
+              "window_starts": "2019-05-19 18:01:00 +0100",
+              "window_finishes": "2019-05-19 18:02:00 +0100",
+              "span": "60"
+          }
+        ]
+      )
     end
   end
 end
-
-=begin
-+[{:span=>1 minute,
-+  :window_finishes=>2019-05-18 18:04:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:03:00.000000000 +0100},
-+ {:span=>1 minute,
-+  :window_finishes=>2019-05-18 18:05:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:04:00.000000000 +0100},
-+ {:span=>5 minutes,
-+  :window_finishes=>2019-05-18 18:10:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:05:00.000000000 +0100},
-+ {:span=>5 minutes,
-+  :window_finishes=>2019-05-18 18:15:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:10:00.000000000 +0100},
-+ {:span=>5 minutes,
-+  :window_finishes=>2019-05-18 18:20:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:15:00.000000000 +0100},
-+ {:span=>5 minutes,
-+  :window_finishes=>2019-05-18 18:25:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:20:00.000000000 +0100},
-+ {:span=>5 minutes,
-+  :window_finishes=>2019-05-18 18:30:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:25:00.000000000 +0100},
-+ {:span=>30 minutes,
-+  :window_finishes=>2019-05-18 19:00:00.000000000 +0100,
-+  :window_starts=>2019-05-18 18:30:00.000000000 +0100},
-+ {:span=>30 minutes,
-+  :window_finishes=>2019-05-18 19:30:00.000000000 +0100,
-+  :window_starts=>2019-05-18 19:00:00.000000000 +0100},
-+ {:span=>30 minutes,
-+  :window_finishes=>2019-05-18 20:00:00.000000000 +0100,
-+  :window_starts=>2019-05-18 19:30:00.000000000 +0100},
-+ {:span=>30 minutes,
-+  :window_finishes=>2019-05-18 20:30:00.000000000 +0100,
-+  :window_starts=>2019-05-18 20:00:00.000000000 +0100},
-+ {:span=>30 minutes,
-+  :window_finishes=>2019-05-18 21:00:00.000000000 +0100,
-+  :window_starts=>2019-05-18 20:30:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 00:00:00.000000000 +0100,
-+  :window_starts=>2019-05-18 21:00:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 03:00:00.000000000 +0100,
-+  :window_starts=>2019-05-19 00:00:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 06:00:00.000000000 +0100,
-+  :window_starts=>2019-05-19 03:00:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 09:00:00.000000000 +0100,
-+  :window_starts=>2019-05-19 06:00:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 12:00:00.000000000 +0100,
-+  :window_starts=>2019-05-19 09:00:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 15:00:00.000000000 +0100,
-+  :window_starts=>2019-05-19 12:00:00.000000000 +0100},
-+ {:span=>3 hours,
-+  :window_finishes=>2019-05-19 18:00:00.000000000 +0100,
-+  :window_starts=>2019-05-19 15:00:00.000000000 +0100},
-+ {:span=>1 minute,
-+  :window_finishes=>2019-05-19 18:01:00.000000000 +0100,
-+  :window_starts=>2019-05-19 18:00:00.000000000 +0100},
-+ {:span=>1 minute,
-+  :window_finishes=>2019-05-19 18:02:00.000000000 +0100,
-+  :window_starts=>2019-05-19 18:01:00.000000000 +0100}]
-=end
