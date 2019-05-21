2741612f48215fec9ca13762412f842764aee975
Rodrigo Estebanez <rodhom@gmail.com>
2019-05-21 11:58:36 +0100

    refactor in more concise way

--- second_approach/rolling_window_spec.rb
+++ second_approach/rolling_window_spec.rb
@@ -10,18 +10,16 @@ require_relative 'rolling_window'
 $redis_store_obj = Redis.new
 
 RSpec.describe "Rolling windows in Redis" do
-  let(:user_id) { 12345 }
-  let(:rolling_window) { RollingWindow.new($redis_store_obj, user_id) }
-  let(:epoch_since) { Time.iso8601("2019-05-18T18:02:29+01:00").to_i }
+  let(:rolling_window) { RollingWindow.new($redis_store_obj, 111) }
+  let(:epoch_since) { Time.parse("2019-05-18 18:02:29 +01:00") }
 
   context "computing one day one day" do
-    let(:one_day_in_seconds) { 60 * 60 * 24}
-    let(:epoch_to) { epoch_since + one_day_in_seconds  }
+    let(:epoch_to) { epoch_since + 1.day  }
 
-    it 'returns the buckes names within the time range' do
-      bucket_names = rolling_window.generate_bucket_names(epoch_since: epoch_since, epoch_to: epoch_to)
-      puts JSON.pretty_generate(bucket_names)
-      expect(JSON.generate(bucket_names)).to include_json(
+    subject { rolling_window.generate_bucket_names(epoch_since: epoch_since, epoch_to: epoch_to) }
+
+    it 'returns the largest possible time span windows within the time range' do
+      expect(JSON.generate(subject)).to include_json(
         [
           {
             "window_starts": "2019-05-18 18:03:00 +0100",
